def503bc49763e7b4fee1b9728c895fa1fd33518
ramprashant66 <ramprashant66@gmail.com>
2025-02-12 02:46:36 -0800

    Added the same function but with class functionality

--- DataProcessing.py
+++ DataProcessing.py
@@ -13,34 +13,34 @@ import pandas
 '''
 This functions reads the data from a .csv file and makes columns for the data
 '''
-def makeTables():
+
+
+class DataPreprocessing:
+    '''
+    This class is used to preprocess the data
+    '''
+    def __init__(self):
+        self.data = None
+
+def makeTables(self):
     try:
         # import the data from the .csv file. "data" = dataframe
-        data = pandas.read_csv('LaqnData.csv')
+        self.data = pandas.read_csv('LaqnData.csv')
 
         #print the data attribute columns
-        print(data.columns)
+        print(self.data.columns)
         
         # print the number of rows and columns
-        print(f'We have {data.shape[0]} rows')
-        print(f'We have {data.shape[1]} columns')
+        print(f'We have {self.data.shape[0]} rows')
+        print(f'We have {self.data.shape[1]} columns')
 
         # print the first 5 rows of the data
-        data.head()
+        self.data.head()
     except Exception as e:
         print("An error occured while trying to read from the CSV file", e)
+ 
 
-
-def main():
+if __name__ == "__main__":
     #import the data file for reading and make columns
-    makeTables()
-
-main()
-
-
-
-
-
-
-
-
+    preprocessedData = DataPreprocessing()
+    makeTables(preprocessedData)
