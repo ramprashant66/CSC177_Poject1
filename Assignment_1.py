223161988745a9fcff524d3557e6cdb553bcaf10
ramprashant66 <ramprashant66@gmail.com>
2025-03-04 03:54:24 -0800

    This is the main file. Please use this for any coding.

---
+++ Assignment_1.py
@@ -0,0 +1,241 @@
+'''
+Names: Prashant Ram
+Course: CSC 177
+Assignment: 1 - Data Preprocessing
+Professor: Chidella
+Details: This project deals with preprocessing data from a .csv file to ready it for
+        data mining.
+'''
+
+import pandas
+import matplotlib.pyplot as matPlot
+import seaborn
+
+class DataPreprocessing:
+    '''
+    This class is used to preprocess the data
+    '''
+    #constructor
+    def __init__(self):
+        self.data = None
+
+def displayFunctionHeader(header):
+    print("\n")
+    print("." * 40, '\n', "." * 40)
+    print(f" " * 5, header, " " * 5)
+    print("." * 40, '\n', "." * 40)
+
+'''
+This function reads the data from a .csv file and makes columns for the data
+'''
+def makeTables(self):
+    #formatting to display steps
+    displayFunctionHeader("Making Tables From CSV")
+
+    try:
+        # import the data from the .csv file. "data" = dataframe
+        self.data = pandas.read_csv('lung_disease_data.csv')
+
+        #print the data attribute columns
+        print(self.data.columns)
+        
+        # print the number of rows and columns
+        print(f'We have {self.data.shape[0]} rows')
+        print(f'We have {self.data.shape[1]} columns')
+
+        # print the first 5 rows of the data
+        print('\n', self.data.head())
+        
+    except Exception as e:
+        print("An error occured while trying to read from the CSV file", e)
+
+'''
+This function counts the number of missing values in each column
+'''
+def missingValuesCount(self):
+    #formatting to display steps
+    displayFunctionHeader("Missing Values In Each Column")
+
+    #list to hold the columns with missing values
+    missingColumns = []
+
+    try:
+        #loop through the columns and get the number of missing values
+        for i in self.data.columns:
+            #get the total of column of missing values
+            total = self.data[i].isnull().sum()
+            #display the column and the missing values number
+            print(f"{i}: {total}")
+
+            #add missing value columns to the list
+            if total > 0:
+                missingColumns.append(i)
+
+        #finally display which columns had missing values
+        print(f"\nThese columns had missing data: {missingColumns}")
+    
+    #catch any reading errors
+    except Exception as e:
+        print("umm. An error occurred while trying to count the missing values..", e)
+
+'''
+This function drops all rows with missing data
+'''
+def dropMissingData(self):
+    displayFunctionHeader("Dropping Rows With Missing Data")
+
+    # print the number of rows and columns
+    print(f'We have {self.data.shape[0]} [original] rows')
+    print(f'We have {self.data.shape[1]} [original] columns')
+     # print the first 5 rows of the data
+    print('\n', self.data.head())
+    
+    #drops all rows with no data
+    self.data = self.data.dropna()
+
+    print(f'We have {self.data.shape[0]} [original] rows')
+    print(f'We have {self.data.shape[1]} [original] columns')
+     # print the first 5 rows of the data
+    print('\n', self.data.head())
+
+'''
+Plots the data from the current dataframe
+'''
+def PlotData(self):
+
+    #choose figure size for the plot area
+    matPlot.figure(figsize=(12, 6))
+    seaborn.boxplot(data=self.data[['Age', 'Lung Capacity', 'Hospital Visits']])
+    matPlot.title("Boxplot of Numeric Columns")
+    #display the data
+    matPlot.show()
+
+'''
+Convert Categorical columns to Numeric
+'''
+def convertCategoricalToNumeric(self):
+    displayFunctionHeader("Converting Categorical Columns to Numeric")
+    genderToNumeric(self)
+    smokingStatusToNumeric(self)
+    recoveredToNumeric(self)
+    disease_TreatmentTypeToNumeric(self)
+
+    # Convert boolean columns to integers 1 for True and 0 for False
+    self.data = self.data.astype(int)
+
+    #save the dataset using the new split data columns for Disease Type and Treatment Type
+    self.data.to_csv('lung_disease_data_numeric.csv', index=False)
+    print('>>>> Data saved to lung_disease_data_numeric.csv')
+    
+
+
+'''
+Convert gender column to numeric
+'''
+def genderToNumeric(self):
+    #convert Gender column
+    self.data['Gender'] = self.data['Gender'].map({'Male': 0, 'Female': 1})
+    print('Gender Column.........DONE')
+
+'''
+Convert Smoking Status Column to Numeric
+'''
+def smokingStatusToNumeric(self):
+    #convert Smoking Status column
+    self.data['Smoking Status'] = self.data['Smoking Status'].map({'Yes': 1, 'No': 0})
+    print('Smoking Status Column.........DONE')
+
+
+'''
+Convert Disease and Treatment Type Column to Numeric
+'''
+def disease_TreatmentTypeToNumeric(self):
+    #convert Disease Type column
+    self.data = pandas.get_dummies(self.data, columns=['Disease Type', 'Treatment Type'], drop_first=True)
+    print('Disease Column.........DONE')
+    print('Treatment Column.........DONE')
+
+'''
+Convert Recovered Column to Numeric
+'''
+def recoveredToNumeric(self):
+    #convert Recover
+    self.data['Recovered'] = self.data['Recovered'].map({'Yes': 1, 'No': 0})
+    print('Recovered Column.........DONE')
+
+
+'''
+This function cleans the data by removing outliers
+through Z-Score calculation (drops data > 3 or < -3)
+'''
+def cleanOutliers(self):
+    displayFunctionHeader("Cleaning Outliers")
+    #convert field to numeric so that they can be used in the zScore calculation
+    #calculate the mean of "Value" column
+    mean = self.data['Value'].mean()
+    #calculate the standard deviation of "Value" column
+    std = self.data['Value'].std()
+    #calulate the zScore
+    zScore = (self.data['Value'] - mean) / std
+
+    #show original data WITH outliers
+    print(f"\nNumer of rows WITH outliers: {zScore.shape[0]}")
+
+    # Create a mask for the all values in 'Values' that are outliers
+    #accounts for +- 3 using normal distribution
+    mask = zScore.abs() <= 3
+
+    # Keep only the rows that pass this condition
+    cleanData = self.data[mask]
+
+    # #remove outliers
+    self.data = cleanData
+    
+    print(f"Numer of rows without outliers: {self.data.shape[0]}")
+    print(f"We removed {zScore.shape[0] - self.data.shape[0]} number of outliers from the original data!\n")
+
+'''
+This function removes duplicates from the data
+'''
+def removeDuplicates(self):
+    displayFunctionHeader("Removing Duplicates")
+
+    #catch how many duplicates were in the data frame
+    duplicatesFound = self.data.shape[0]
+    print(f"\nNumer of rows WITH duplicates: {self.data.shape[0]}")
+
+    #drop duplicates
+    self.data = self.data.drop_duplicates(keep = False)
+
+    print(f"\nNumer of rows WITHOUT duplicates: {self.data.shape[0]}")
+    print(f"\nThere were {duplicatesFound - self.data.shape[0]} duplicates in the data frame")
+    
+
+
+if __name__ == "__main__":
+    #make the class object to work with
+    preprocessedData = DataPreprocessing()
+
+    #import the data file for reading and make columns
+    makeTables(preprocessedData)
+
+    #check for missing values
+    missingValuesCount(preprocessedData)
+    
+    #drop all rows with missing data
+    dropMissingData(preprocessedData)
+
+    #remove duplicates
+    removeDuplicates(preprocessedData)
+
+    #convert categorical columns to numeric
+    convertCategoricalToNumeric(preprocessedData)
+
+    #clean the data by removing outliers
+    # cleanOutliers(preprocessedData)
+
+   
+
+    # Plots the current data from the dataframe
+    PlotData(preprocessedData)
+   
